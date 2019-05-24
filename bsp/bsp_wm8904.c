1893cadbdcf12b73f9e9eaf85ef93e0cab708eeb
xiaoming <2014500726@smail.xtu.edu.cn>
2019-05-24 21:36:34 +0800

    update code

--- bsp/bsp_wm8904.c
+++ bsp/bsp_wm8904.c
@@ -20,12 +20,6 @@
 #include "fsl_i2s_dma.h"
 
 
-#define I2S_DMA_TX  15
-#define I2S_DMA_RX  12
-
-#define BOARD_CODEC_I2C_BASEADDR   I2C4
-#define BOARD_CODEC_I2C_CLOCK_FREQ 12000000
-
 dma_handle_t s_DmaTxHandle;
 dma_handle_t s_DmaRxHandle;
 static i2s_config_t s_TxConfig;
@@ -44,7 +38,7 @@ const pll_setup_t pllSetup =
 
 
 
-void BOARD_I2C_Init(I2C_Type *base, uint32_t clkSrc_Hz)
+void WM8904_I2C_Init(I2C_Type *base, uint32_t clkSrc_Hz)
 {
     i2c_master_config_t i2cConfig = {0};
 
@@ -52,7 +46,7 @@ void BOARD_I2C_Init(I2C_Type *base, uint32_t clkSrc_Hz)
     I2C_MasterInit(base, &i2cConfig, clkSrc_Hz);
 }
 
-status_t BOARD_I2C_Send(I2C_Type *base,
+status_t WM8904_I2C_Send(I2C_Type *base,
                         uint8_t deviceAddress,
                         uint32_t subAddress,
                         uint8_t subaddressSize,
@@ -73,7 +67,7 @@ status_t BOARD_I2C_Send(I2C_Type *base,
     return I2C_MasterTransferBlocking(base, &masterXfer);
 }
 
-status_t BOARD_I2C_Receive(I2C_Type *base,
+status_t WM8904_I2C_Receive(I2C_Type *base,
                            uint8_t deviceAddress,
                            uint32_t subAddress,
                            uint8_t subaddressSize,
@@ -94,28 +88,28 @@ status_t BOARD_I2C_Receive(I2C_Type *base,
     return I2C_MasterTransferBlocking(base, &masterXfer);
 }
 
-void BOARD_Codec_I2C_Init(void)
+void WM8904_Codec_I2C_Init(void)
 {
-    BOARD_I2C_Init(BOARD_CODEC_I2C_BASEADDR, BOARD_CODEC_I2C_CLOCK_FREQ);
+    WM8904_I2C_Init(WM8904_I2C, 12000000);
 }
 
-status_t BOARD_Codec_I2C_Send(
+status_t WM8904_Codec_I2C_Send(
     uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, const uint8_t *txBuff, uint8_t txBuffSize)
 {
-    return BOARD_I2C_Send(BOARD_CODEC_I2C_BASEADDR, deviceAddress, subAddress, subAddressSize, (uint8_t *)txBuff,
+    return WM8904_I2C_Send(WM8904_I2C, deviceAddress, subAddress, subAddressSize, (uint8_t *)txBuff,
                           txBuffSize);
 }
 
-status_t BOARD_Codec_I2C_Receive(
+status_t WM8904_Codec_I2C_Receive(
     uint8_t deviceAddress, uint32_t subAddress, uint8_t subAddressSize, uint8_t *rxBuff, uint8_t rxBuffSize)
 {
-    return BOARD_I2C_Receive(BOARD_CODEC_I2C_BASEADDR, deviceAddress, subAddress, subAddressSize, rxBuff, rxBuffSize);
+    return WM8904_I2C_Receive(WM8904_I2C, deviceAddress, subAddress, subAddressSize, rxBuff, rxBuffSize);
 }
 
 codec_config_t boardCodecConfig = 
 {
-	.I2C_SendFunc = BOARD_Codec_I2C_Send,
-	.I2C_ReceiveFunc = BOARD_Codec_I2C_Receive,
+	.I2C_SendFunc = WM8904_Codec_I2C_Send,
+	.I2C_ReceiveFunc = WM8904_Codec_I2C_Receive,
 	.op.Init = WM8904_Init,
 	.op.Deinit = WM8904_Deinit,
 	.op.SetFormat = WM8904_SetAudioFormat
@@ -147,7 +141,7 @@ status_t wm8904_i2s_init(void)
 	
 	
 	PRINTF("Configure I2C\r\n");
-    BOARD_Codec_I2C_Init();
+    WM8904_Codec_I2C_Init();
 
 	PRINTF("Configure WM8904 codec\r\n");
 	/*
@@ -182,12 +176,12 @@ status_t wm8904_i2s_init(void)
 	I2S_RxInit(WM8904_I2S_RX, &s_RxConfig);
 
 	/* 配置使能IISDMA */
-	DMA_EnableChannel(DMA0, I2S_DMA_TX);
-	DMA_EnableChannel(DMA0, I2S_DMA_RX);
-	DMA_SetChannelPriority(DMA0, I2S_DMA_TX, kDMA_ChannelPriority3);
-	DMA_SetChannelPriority(DMA0, I2S_DMA_RX, kDMA_ChannelPriority2);
-	DMA_CreateHandle(&s_DmaTxHandle, DMA0, I2S_DMA_TX);
-	DMA_CreateHandle(&s_DmaRxHandle, DMA0, I2S_DMA_RX);
+	DMA_EnableChannel(DMA0, WM8904_I2S_DMA_TX);
+	DMA_EnableChannel(DMA0, WM8904_I2S_DMA_RX);
+	DMA_SetChannelPriority(DMA0, WM8904_I2S_DMA_TX, kDMA_ChannelPriority3);
+	DMA_SetChannelPriority(DMA0, WM8904_I2S_DMA_RX, kDMA_ChannelPriority2);
+	DMA_CreateHandle(&s_DmaTxHandle, DMA0, WM8904_I2S_DMA_TX);
+	DMA_CreateHandle(&s_DmaRxHandle, DMA0, WM8904_I2S_DMA_RX);
     
 	return kStatus_Success;
 }
--- bsp/bsp_wm8904.h
+++ bsp/bsp_wm8904.h
@@ -19,8 +19,12 @@
 #include "fsl_i2s_dma.h"
 #include "fsl_wm8904.h"
 
-#define WM8904_I2S_TX  I2S1
-#define WM8904_I2S_RX  I2S0
+
+#define WM8904_I2C         I2C4
+#define WM8904_I2S_TX      I2S1
+#define WM8904_I2S_RX      I2S0
+#define WM8904_I2S_DMA_TX  15
+#define WM8904_I2S_DMA_RX  12
 
 extern dma_handle_t s_DmaTxHandle;
 extern dma_handle_t s_DmaRxHandle;
